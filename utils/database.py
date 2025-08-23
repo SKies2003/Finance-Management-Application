5ab6414f7a1408e30529d76c1b8a25f7e0150c6c
Gagan Singhal <gagansinghal1729@gmail.com>
2025-08-24 01:28:45 +0530

    Updated User registration and its corresponding table and Added new transactions table with its function

--- utils/database.py
+++ utils/database.py
@@ -1,54 +1,65 @@
 import sqlite3
+from typing import Optional
 
 class DatabaseManager:
     def __init__(self, db_name = "db.sqlite"):
         self.conn = sqlite3.connect(db_name)
         self.cursor = self.conn.cursor()
+        self.cursor.execute("PRAGMA foreign_keys = ON")
         self.cursor.execute("""
         CREATE TABLE IF NOT EXISTS authenticate (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             username TEXT UNIQUE,
-            password TEXT
+            password TEXT,
+            name TEXT,
+            age INTEGER,
+            gender TEXT CHECK(gender IN ('Male', 'Female')),
+            contact_no TEXT UNIQUE,
+            pan TEXT unique
         )
         """)
+
         self.cursor.execute("""
-            CREATE TABLE IF NOT EXISTS profile (
+            CREATE TABLE IF NOT EXISTS transactions (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name TEXT,
-                age INTEGER,
-                gender TEXT CHECK(gender IN ('Male', 'Female')),
-                mobile_number TEXT UNIQUE,
-                pan TEXT UNIQUE
+                user_id INTEGER,
+                transaction_type TEXT CHECK(transaction_type IN ('Income', 'Expense')),
+                category TEXT,
+                amount REAL,
+                date TEXT DEFAULT CURRENT_DATE,
+                FOREIGN KEY (user_id) REFERENCES authenticate(id) ON DELETE CASCADE
             )
             """)
 
-    def sign_up(self, username: str, password: str):
+    def sign_up(self, username: str, password: str, name: str, age: int, gender: str, contact_no: str, pan: str):
         try:
-            self.cursor.execute("INSERT INTO authenticate (username, password) VALUES (?, ?)", (username, password))
+            self.cursor.execute("INSERT INTO authenticate (username, password, name, age, gender, contact_no, pan) VALUES (?, ?, ?, ?, ?, ?, ?)", (username, password, name, age, gender, contact_no, pan))
             self.conn.commit()
+            user_id = self.cursor.lastrowid
             print("User registered successfully!")
-            return True
+            return user_id
         except sqlite3.IntegrityError:
             print("User with this username already exists!")
-            return False
-
-    def create_profile(self, name: str, age: int, gender: str, mobile_number: str, pan: str):
-        try:
-            self.cursor.execute("INSERT INTO profile (name, age, gender, mobile_number, pan) VALUES (?, ?, ?, ?, ?)", (name, age, gender, mobile_number, pan))
-            self.conn.commit()
-            return True
-        except sqlite3.IntegrityError:
-            print("User with this mobile number/pan already exists!")
-            return False
+            return None
     
     def login(self, username: str, password: str):
         try:
             self.cursor.execute("SELECT password FROM authenticate WHERE username = ?", (username,))
             db_password = self.cursor.fetchone()
-            if db_password is not None:
-                return db_password[0] == password
-        except TypeError:
+            if db_password[0] == password:
+                self.cursor.execute("SELECT id FROM authenticate WHERE username = ?", (username,))
+                user_id = self.cursor.fetchone()
+                return user_id[0]
+            else:
+                print("Invalid password!")
+        except:
+            print("user not found!")
             return None
+
+    def add_transaction(self, user_id: int, transaction_type: str, category: str, amount: float):
+        self.cursor.execute("INSERT INTO transactions (user_id, transaction_type, category, amount) VALUES (?, ?, ?, ?)", (user_id, transaction_type, category, amount))
+        self.conn.commit()
+        return True
     
     def close(self):
         self.cursor.close()
