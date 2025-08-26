d6d4045a005ea235954d247543240cc0f050ffc2
Gagan Singhal <gagansinghal1729@gmail.com>
2025-08-26 20:48:29 +0530

    Code refactored, DRY principle applied

--- services/authenication.py
+++ services/authenication.py
@@ -3,56 +3,69 @@ from utils.database import DatabaseManager
 class UserAuthenticate():
     def __init__(self, db: DatabaseManager):
         self.db = db
-
-    def user_registration(self):
+    
+    def username(self):
         username = input("Enter a unique username: ").strip()
         while len(username) < 6:
             print("Invalid username! Must be greater than 5 characters.\n")
             username = input("Enter a unique username: ").strip()
-        
+        return username
+    
+    def password(self):
         password = input("Enter a password: ").strip()
         while len(password) < 6:
             print("Invalid password! Must be greater than 5 characters.\n")
             password = input("Enter a password: ").strip()
-        
+        return password
+    
+    def name(self):
         name = input("Enter your name: ").strip()
         while len(name) < 3:
             print("Name must contain more than 2 characters.\n")
             name = input("Enter your name: ").strip()
-
+        return name
+    
+    def age(self):
         while True:
             try:
                 age = int(input("Enter your age: "))
                 break
             except ValueError:
                 print("Age must be numbers not characters!\n")
-
+        return age
+    
+    def gender(self):
         gender = input("Enter Male/Female: ").capitalize()
         while gender not in ["Male", "Female"]:
             print("Invalid gender! Please enter Male or Female.\n")
             gender = input("Enter Male/Female: ").capitalize()
-        
+        return gender
+    
+    def mobile_no(self):
         mobile_no = input("Enter your contact number: ")
         while not (mobile_no.isdigit() and len(mobile_no) == 10):
             print("Invalid contact number! Must be 10 digts.\n")
             mobile_no = input("Enter your contact number: ")
-        
+        return mobile_no
+    
+    def pan(self):
         pan = input("Enter your 10 digit PAN Number: ")
         while not (len(pan) == 10 and pan[:5].isupper() and pan[6:9].isdigit() and pan[-1].isupper()):
             print("Invalid PAN id provided! Please enter correct PAN ID.\n")
             pan = input("Enter your 10 digit PAN Number: ")
-        
+        return pan
+
+    def user_registration(self):
+        username = self.username()
+        password = self.password()
+        name = self.name()
+        age = self.age()
+        gender = self.gender()
+        mobile_no = self.mobile_no()
+        pan = self.pan()
         return self.db.sign_up(username, password, name, age, gender, mobile_no, pan)
     
     def user_login(self):
-        username = input("Enter a unique username: ").strip()
-        while len(username) < 6:
-            print("Invalid username! Must be greater than 5 characters.\n")
-            username = input("Enter a unique username: ").strip()
-        
-        password = input("Enter a password: ").strip()
-        while len(password) < 6:
-            print("Invalid password! Must be greater than 5 characters.\n")
-            password = input("Enter a password: ").strip()
-        
+        username = self.username()
+        password = self.password()
         return self.db.login(username, password)
--- services/tracker.py
+++ services/tracker.py
@@ -4,21 +4,51 @@ class Tracker:
     def __init__(self, db: DatabaseManager):
         self.db = db
     
-    def add_transaction(self, user_id: int):
+    def transaction_type(self):
         income_expense = input("Enter type of transaction made (Income/Expense): ").strip().capitalize()
         lst = ["Income", "Expense"]
         while income_expense not in lst:
             print("Invalid transaction type received!")
             income_expense = input("Enter type of transaction made (Income/Expense): ").strip().capitalize()
-        
-        category = input("Enter category of transaction made (food, shopping, salary, profit etc.): ").strip().lower()
-
+        return income_expense
+    
+    def category(self):
+        while True:
+            try:
+                category = input("Enter category (food, salary, fuel, travel, food, profit): ").strip().lower()
+                if len(category) == 0:
+                    print("category cannot be empty")
+                else:
+                    break
+            except ValueError:
+                print("category cannot be empty")
+        return category
+    
+    def amount(self):
         while True:
             try:
                 amount = float(input("Enter the amount of transaction made: "))
                 break
             except ValueError:
                 print("Invalid value found for column amount!")
+        return amount
+    
+    def transaction_id(self):
+        while True:
+            try:
+                transaction_id = int(input("Enter your transaction id: "))
+                if transaction_id < 1:
+                    print("transaction id must be a postive integer.")
+                else:
+                    break
+            except ValueError:
+                print("transaction id must be a postive integer.")
+        return transaction_id
+    
+    def add_transaction(self, user_id: int):
+        income_expense = self.transaction_type()
+        category = self.category()
+        amount = self.amount()
         print(f"\nYour {income_expense} of {amount} for {category} is saved.")
         return self.db.add_transaction(user_id, income_expense, category, amount)
     
@@ -26,33 +56,34 @@ class Tracker:
         return self.db.display_transactions(user_id)
 
     def update_transaction(self):
-        transaction_id = int(input("Enter your transaction id: "))
-        choice = int(input("What u want to update \n1. type of transaction \n2. category \n3. amount ? "))
+        transaction_id = self.transaction_id()
+
+        while True:
+            try:
+                choice = int(input("What u want to update \n1. type of transaction \n2. category \n3. amount ? "))
+                if choice < 1 or choice > 3:
+                    print("Choose from given options.")
+                else:
+                    break
+            except ValueError:
+                print("Invalid input! Enter 1, 2 or 3 only.")
+
         if choice == 1:
-            income_expense = input("Enter type of transaction made (Income/Expense): ").strip().capitalize()
-            lst = ["Income", "Expense"]
-            while income_expense not in lst:
-                print("Invalid transaction type received!")
-                income_expense = input("Enter type of transaction made (Income/Expense): ").strip().capitalize()
+            income_expense = self.transaction_type()
             print("Your transaction with id", transaction_id, "has been updated as", income_expense)
             return self.db.update_transaction(transaction_id, "transaction_type", income_expense)
         
         if choice == 2:
-            category = input("Enter category of transaction made (food, shopping, salary, profit etc.): ").strip().lower()
+            category = self.category()
             print("Your transaction with id", transaction_id, "has been updated as", category)
             return self.db.update_transaction(transaction_id, "category", category)
         
         if choice == 3:
-            while True:
-                try:
-                    amount = float(input("Enter the amount of transaction made: "))
-                    break
-                except ValueError:
-                    print("Invalid value found for column amount!")
+            amount = self.amount()
             print("Amount for your transaction with id", transaction_id, "has been updated to", amount)
             return self.db.update_transaction(transaction_id, "amount", amount)
         
     def delete_transaction(self):
-        transaction_id = int(input("Enter your transaction id: "))
+        transaction_id = self.transaction_id()
         print("Your transaction with id", transaction_id, "has been deleted!")
         return self.db.delete_transaction(transaction_id)
